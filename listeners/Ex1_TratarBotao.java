6ecac754dba8167df7a208aa3d4a3a693fe7bcd0
Gabriel <gabriel.mazer@outlook.com>
2024-04-11 20:32:55 -0300

    Ex1 ActionListener

--- listeners/Ex1_TratarBotao.java
+++ listeners/Ex1_TratarBotao.java
@@ -3,14 +3,20 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Ex1 extends JFrame implements ActionListener{
+class TratarBotao implements ActionListener{
+    public void actionPerformed(ActionEvent e){
+        System.out.println("O botão foi clicado");
+    }
+}
+
+public class Ex1_TratarBotao extends JFrame{
     JButton bt = new JButton("Botão");
 
-    Ex1() {
+    Ex1_TratarBotao() {
         super("Teste ActionListener");
         setLayout(new FlowLayout());
         
-        bt.addActionListener(this);
+        bt.addActionListener(new TratarBotao());
         add(bt);
 
         setVisible(true);
@@ -19,13 +25,11 @@ public class Ex1 extends JFrame implements ActionListener{
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        System.out.println("O botão foi clicado");
         
-    }
 
     public static void main(String[] args){
         new Ex1();
     }
-}
+} 
+    
+
--- listeners/Ex2.java
+++ listeners/Ex2.java
@@ -3,15 +3,17 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Ex1 extends JFrame implements ActionListener{
+public class Ex2 extends JFrame implements ActionListener{
     JButton bt = new JButton("Botão");
+    JTextField text1 = new JTextField(20);
 
-    Ex1() {
+    Ex2() {
         super("Teste ActionListener");
         setLayout(new FlowLayout());
         
         bt.addActionListener(this);
         add(bt);
+        add(text1);
 
         setVisible(true);
         pack();
@@ -19,13 +21,13 @@ public class Ex1 extends JFrame implements ActionListener{
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
-    @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.println("O botão foi clicado");
+        String t1 = text1.getText();
+        System.out.println(t1);
         
     }
 
     public static void main(String[] args){
-        new Ex1();
+        new Ex2();
     }
 }
